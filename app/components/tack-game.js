3d9f2b9889b7bc8edbc6b3a9ba6f3a1017643f4a
SilverBirchh <bradleybirch09@yahoo.co.uk>
2017-05-14 20:54:03 +0100

    not sure

--- app/components/tack-game.js
+++ app/components/tack-game.js
@@ -2,73 +2,81 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-      playerOne: {
-        name: 'Player One',
-        shape: 'circle',
-        score: '0'
-      },
-      playerTwo: {
-        name: 'Player Two',
-        shape: 'cross',
-        score: '0'
-      },
+  playerOne: {
+    name: 'Player One',
+    shape: 'circle',
+    score: '0'
+  },
+  playerTwo: {
+    name: 'Player Two',
+    shape: 'cross',
+    score: '0'
+  },
 
-      nextGo: Ember.computed('isPlayerOnesGo', 'playerOne.name', function() {
-        const nextPlayer = this.get('isPlayerOnesGo') ? this.get('playerOne.name') : this.get('playerTwo.name');
-        return `${nextPlayer} is next`
-      }),
+  nextGo: Ember.computed('isPlayerOnesGo', 'playerOne.name', function() {
+    const nextPlayer = this.get('isPlayerOnesGo') ? this.get('playerOne.name') : this.get('playerTwo.name');
+    return `${nextPlayer} is next`
+  }),
 
-      shapes: ['circle', 'cross', 'dino'],
+  shapes: ['circle', 'cross', 'dino'],
 
-      isPlayerOnesGo: true,
+  isPlayerOnesGo: true,
 
-      disabled: false,
+  disabled: false,
 
-      result: null,
+  result: null,
 
-      squares: Ember.A(new Array(9).fill(null)),
+  squares: Ember.A(new Array(9).fill(null)),
 
-      wasADraw() {
-        const squares = this.get('squares');
-        if (squares.includes(null)) {
-          return false;
-        }
-        return true;
-      },
+  wasADraw() {
+    const squares = this.get('squares');
+    if (squares.includes(null)) {
+      return false;
+    }
+    return true;
+  },
 
-      winner() {
-        const rows = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
-        for (let i = 0; i < rows.length; i++) {
-          const [a, b, c] = rows[i];
-          const squares = this.get('squares');
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-      },
+  winner() {
+    const rows = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < rows.length; i++) {
+      const [a, b, c] = rows[i];
+      const squares = this.get('squares');
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+  },
 
-      checkResult() {
-            if (this.winner()) {
-              let player = this.get('playerOne.shape') === this.winner() ?
-                'playerOne' : 'playerTwo';
-              let score = parseInt(this.get(`${player}.score`));
-              this.set(`${player}.score`, ++score);
-              this.set('result', `The winner was ${this.get(`${player}.name`)}! Well done!`);
-   }
+  checkResult() {
+    this.sendAction('saveResult');
+    this.sendAction('editResult');
+    if (this.winner()) {
+      let player = this.get('playerOne.shape') === this.winner() ?
+        'playerOne' : 'playerTwo';
+      let score = parseInt(this.get(`${player}.score`));
+      this.set(`${player}.score`, ++score);
+      this.set('result', `The winner was ${this.get(`${player}.name`)}! Well done!`);
+    } else if (this.wasADraw()) {
+      this.set('result', 'Too Bad. It was a draw!');
+    }
+  },
 
-   else if (this.wasADraw()) {
-     this.set('result', 'Too Bad. It was a draw!');
-   }
- },
+  saveResult() {
+    this.sendAction('saveResult');
+  },
+
+  editResult() {
+    this.sendAction('editResult');
+  },
 
   actions: {
     select(squareId) {
@@ -93,6 +101,6 @@ export default Ember.Component.extend({
         result: null,
         squares: Ember.A(new Array(9).fill(null)),
       });
-    }
+    },
   }
 });
--- app/routes/play.js
+++ app/routes/play.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('results');
+  },
+
+  actions: {
+    saveResult(result) {
+      console.log("Hello");
+    },
+
+    editResult(result) {
+      console.log("byeee");
+    },
+  }
+
 });
